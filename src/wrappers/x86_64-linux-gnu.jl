2f556b58caf1a09850bd715f84b85aff964e9a62
Tim Besard <tim.besard@gmail.com>
2024-04-12 14:13:57 +0200

    oneAPI_Support_jll build 0.4.0+0

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -2,10 +2,11 @@
 export liboneapi_support
 
 using oneAPI_Level_Zero_Loader_jll
+using OpenCL_jll
 JLLWrappers.@generate_wrapper_header("oneAPI_Support")
 JLLWrappers.@declare_library_product(liboneapi_support, "liboneapi_support.so")
 function __init__()
-    JLLWrappers.@generate_init_header(oneAPI_Level_Zero_Loader_jll)
+    JLLWrappers.@generate_init_header(oneAPI_Level_Zero_Loader_jll, OpenCL_jll)
     JLLWrappers.@init_library_product(
         liboneapi_support,
         "lib/liboneapi_support.so",
